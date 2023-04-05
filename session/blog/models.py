43f90508199400328f9ae188f066994f1a9e97a0
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-04-05 21:25:46 +0900

    feat: first init

---
+++ session/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog/', null=True)
+
+    class Meta:
+        db_table = 'blog'
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.content[:100]
