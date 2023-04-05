b6bb81726526602155c3e7db64062a7c4f455e82
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-04-05 23:21:48 +0900

    feat: 1대1 관계로 블로그 글에 유저 연결하기

--- session/blog/models.py
+++ session/blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', null=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
         db_table = 'blog'
     
     def __str__(self):
-        return self.title
+        return self.title + ' | ' + str(self.author)
     
     def summary(self):
         return self.content[:100]
