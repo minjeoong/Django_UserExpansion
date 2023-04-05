311a0d32975683552091f2848c661b02325d1a02
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-04-05 23:22:24 +0900

    feat: 유저 정보 받아와서 처리하기

--- session/blog/views.py
+++ session/blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.core.paginator import Paginator
 from .models import Blog
-from .forms import BlogForm
 
 
 def home(request):
@@ -23,22 +22,17 @@ def create(request):
     new_blog.title = request.POST.get('title')
     new_blog.content = request.POST.get('content')
     new_blog.image = request.FILES.get('image')
+    new_blog.author = request.user
     new_blog.save()
     return redirect('detail', new_blog.id)
-    # return render(request, 'detail.html', {'blog':new_blog})
-
-# def create(request):
-#     form = BlogForm(request.POST)
-#
-#     if form.is_valid():
-#         new_blog = form.save(commit=False)
-#         new_blog.save()
-#         return redirect('detail', new_blog.id)
-#
-#     return render(request, 'new.html')
 
 def edit(request, blog_id):
-    edit_blog = get_object_or_404(Blog, pk=blog_id) # print() 해보기
+    # edit_blog = get_object_or_404(Blog, pk=blog_id)
+    edit_blog = Blog.objects.get(id=blog_id)
+
+    if request.user != edit_blog.author:
+        return redirect('home')
+    
     return render(request, 'edit.html', {'edit_blog':edit_blog})
 
 
@@ -49,18 +43,6 @@ def update(request, blog_id):
     old_blog.image = request.FILES.get('image')
     old_blog.save()
     return redirect('detail', old_blog.id)
-
-# def update(request, blog_id):
-#     old_blog = get_object_or_404(Blog, pk=blog_id)
-#     form = BlogForm(request.POST, instance=old_blog)
-
-    # 클라이언트가 유효한 값을 입력한 경우
-    if form.is_valid():
-        new_blog = form.save(commit=False)
-        new_blog.save()
-        return redirect('detail', old_blog.id)
-
-    return render(request, 'new.html', {'old_blog':old_blog})
 
 
 def delete(request, blog_id):
